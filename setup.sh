fb00c1db3b9e934aed8d3d086b1b4d2f7e851480
Dexter Shepherd <dexterjshepherd@gmail.com>
2018-03-11 17:34:12 -0700

    first commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get install vim git
+curl https://raw.githubusercontent.com/DexterShepherd/dotfiles/master/vimrc.pi > ~/.vimrc
+curl https://processing.org/download/install-arm.sh | sudo sh

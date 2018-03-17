0e16002ab0be5a25318695cc5e5fd3b0f40af35f
Dexter Shepherd <dexterjshepherd@gmail.com>
2018-03-17 12:46:17 -0400

    adding to install script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@ sudo apt-get clean
 sudo apt-get update
 sudo apt-get install vim git tmate
 curl https://raw.githubusercontent.com/DexterShepherd/dotfiles/master/vimrc.pi > ~/.vimrc
-curl https://processing.org/download/install-arm.sh | sudo sh
+
+if ! [ -x "$(command -v processing-java)" ]; then
+  echo 'installing processing-java' >&2
+  curl https://processing.org/download/install-arm.sh | sudo sh
+fi
+
